cbc109d026144fab448655341f47a72ea5e4d4b6
SusannahAndersson <susannah.alx@gmail.com>
2022-09-26 13:58:28 +0200

    hello world init

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -1,44 +1,21 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-
-//namespace MvcMovie.Controllers
-//{
-//    public class HelloWorldController : Controller
-//    {
-//        public IActionResult Index()
-//        {
-//            return View();
-//        }
-//    }
-//}
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
 
 namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        //
-        // GET: /HelloWorld/
-
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
 
-        ////
-        //// GET: /HelloWorld/Welcome/
-
-        //public string Welcome()
-        //{
-        //    return "This is the Welcome action method...";
-        //}
-
-        ///port/HelloWorld/Welcome?name=Rick&numtimes=4
-        // GET: /HelloWorld/Welcome/
-        // Requires using System.Text.Encodings.Web;
-        public string Welcome(string name, int numTimes = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
 }
